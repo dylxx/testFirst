9587e14c60f9b5c383e2d969e56bda90ffa05745
dylinkxx <15125099021@163.com>
2021-11-08 20:08:17 +0800

    second commit

--- src/main/java/com/dylink/domain/Student.java
+++ src/main/java/com/dylink/domain/Student.java
@@ -4,6 +4,7 @@ public class Student {
     private String id;
     private String name;
     private Integer age;
+    private String address;
 
     public String getId() {
         return id;
@@ -27,6 +28,14 @@ public class Student {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     @Override
